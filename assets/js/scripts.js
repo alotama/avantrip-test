41da2470eb314d0c994f32e83b93d7752102aa4d
Sebastián Tamashiro <sebastian@alotama.com>
2019-02-20 18:15:30 -0300

    Graph scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,32 +1,37 @@
-var $carousel = $('.graph__main').flickity({
-  asNavFor: ".graph__nav",
-  contain: true,
-  pageDots: false,
-  draggable: false,
-  accessibility: false,
-  prevNextButtons: false
-});
+$(document).ready(function () {
+  $('.graph__nav').off('click', '.graph__cell');
 
-$('.graph__nav').on('click', '.graph__cell', function () {
-  
-  // On click check each graph__cell if have class 'is-selected'. 
-  $('.graph__nav .graph__cell').each(function(){
-    if ($(this).hasClass('is-selected')) {
-
-      // If someone have it, then remove class 'is-selected'.
-      $(this).removeClass('is-selected');
-    }
+  var $graph__menu = $('.graph__main').flickity({
+    asNavFor: ".graph__nav",
+    contain: true,
+    pageDots: false,
+    draggable: false,
+    accessibility: false,
+    prevNextButtons: false
   });
 
-  // Add the class 'is-selected' to the 'graph__cell' selected previous.
-  $(this).addClass('is-selected');
+  $('.graph__nav').on('click', '.graph__cell', function () {
 
-  var index = $(this).index(); 
-  $carousel.flickity('select', index);
-});
+    // On click check each graph__cell if have class 'is-selected'.
+    $('.graph__nav .graph__cell').each(function(){
+      if ($(this).hasClass('is-selected')) {
 
-$('.graph__nav').flickity({
-  contain: true,
-  pageDots: false,
-  initialIndex: 5,
+        // If someone have it, then remove class 'is-selected'.
+        $(this).removeClass('is-selected is-nav-selected');
+      }
+    });
+
+    // Add the class 'is-selected' to the 'graph__cell' selected previous.
+    $(this).addClass('is-selected is-nav-selected');
+    var index = $(this).index();
+    $graph__menu.flickity('select', index);
+
+  });
+
+  $('.graph__nav').flickity({
+    contain: true,
+    pageDots: false,
+    draggable: false,
+    initialIndex: 5,
+  });
 });
